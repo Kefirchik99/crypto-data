425ed9b066fe36d2d74b70ae89e560eb6b3cfc50
Kefirchik99 <jermakent@gmail.com>
2024-09-11 20:48:29 +0300

    V 1.0 created basic skeleton

---
+++ src/GlobalData.jsx
@@ -0,0 +1,21 @@
+import Table from 'react-bootstrap/Table';
+
+function GlobalData() {
+    return (
+        <Table >
+            <tbody>
+                <tr>
+                    <td>BTC Dominance:</td>
+                </tr>
+                <tr>
+                    <td>Vol 24h:</td>
+                </tr>
+                <tr>
+                    <td>Market Cap:</td>
+                </tr>
+            </tbody>
+        </Table>
+    );
+}
+
+export default GlobalData;
