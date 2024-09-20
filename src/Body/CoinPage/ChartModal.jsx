0520420d525d01c54dc994f9d2dd0c4dcba5d165
Kefirchik99 <jermakent@gmail.com>
2024-09-20 18:55:25 +0300

    zoom chart modal

---
+++ src/Body/CoinPage/ChartModal.jsx
@@ -0,0 +1,26 @@
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function ChartModal({ show, handleClose, children }) {
+
+    return (
+
+        <Modal size="xl" show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Bitcoin</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {children}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+
+    );
+}
+
+export default ChartModal;
