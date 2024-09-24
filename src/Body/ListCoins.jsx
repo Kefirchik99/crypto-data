4cf87b2e696c38ffffcce206c20fcd99e6204d35
Kefirchik99 <jermakent@gmail.com>
2024-09-24 16:34:16 +0300

    added more global data

--- src/Body/ListCoins.jsx
+++ src/Body/ListCoins.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
 import { getCoinList } from '../services/api';
+import { getGlobalData } from '../services/api';
+
 
 function ListCoins() {
   const [coinList, setCoinList] = React.useState([]);
+  const [globalData, setGlobalData] = React.useState({});
 
   React.useEffect(() => {
     getCoinList().then(data => {
       setCoinList(data.slice(0, 100));
+    });
+  }, []);
+
+  React.useEffect(() => {
+    getGlobalData().then(data => {
+      setGlobalData(data);
     });
   }, []);
 
@@ -33,17 +42,16 @@ function ListCoins() {
             <td>{coin.name}</td>
             <td></td>
             <td></td>
+            <td>{globalData.volume_24h_ath_value}</td>
             <td></td>
+            <td>{globalData.volume_24h_change_24h}</td>
             <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>{globalData.market_cap_ath_value}</td>
           </tr>
         ))}
       </tbody>
     </Table>
-  )
-}
+  );
+};
 
 export default ListCoins;
