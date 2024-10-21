184671ce6c73807f82772bc9a2cca9a97e0212cc
Kefirchik99 <jermakent@gmail.com>
2024-10-21 19:53:03 +0300

    added new component

--- src/Body/GlobalData.jsx
+++ src/Body/GlobalData.jsx
@@ -5,6 +5,10 @@ import Converter from "../Body/CoinPage/Converter";
 import Table from 'react-bootstrap/Table';
 import "../styles/GlobalData.scss";
 import "../styles/Converter.scss";
+import LearnMore from "./LearnMore";
+import "../styles/LearnMore.scss"
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function GlobalData() {
     const [globalData, setGlobalData] = React.useState({});
@@ -16,37 +20,43 @@ function GlobalData() {
 
     return (
         <div className="global-converter-container">
-            <div className="global-data">
-                <Table className="global-data-table table table-striped table-bordered table-hover">
+            <Row className="mb-3">
+                <Col md={4} className="global-data">
+                    <Table className="global-data-table table table-striped table-bordered table-hover">
+                        <tbody>
+                            <tr>
+                                <td colSpan="2">
+                                    <h3>Bitcoin data</h3>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>BTC Dominance</td>
+                                <td>{globalData.bitcoin_dominance_percentage} %</td>
+                            </tr>
+                            <tr>
+                                <td>Vol 24h</td>
+                                <td>{globalData.volume_24h_usd}</td>
+                            </tr>
+                            <tr>
+                                <td>Market Cap</td>
+                                <td>{globalData.market_cap_usd}</td>
+                            </tr>
+                            <tr>
+                                <td>Exchanges count</td>
+                                <td>{exchangeList.length}</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Col>
 
-                    <tbody>
-                        <tr>
-                            <td colSpan="2">
-                                <h3>Bitcoin data</h3>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>BTC Dominance</td>
-                            <td>{globalData.bitcoin_dominance_percentage} %</td>
-                        </tr>
-                        <tr>
-                            <td>Vol 24h</td>
-                            <td>{globalData.volume_24h_usd}</td>
-                        </tr>
-                        <tr>
-                            <td>Market Cap</td>
-                            <td>{globalData.market_cap_usd}</td>
-                        </tr>
-                        <tr>
-                            <td>Exchanges count</td>
-                            <td>{exchangeList.length}</td>
-                        </tr>
-                    </tbody>
-                </Table>
-            </div>
-            <div className="converter">
-                <Converter />
-            </div>
+                <Col md={4}>
+                    <LearnMore />
+                </Col>
+
+                <Col md={4}>
+                    <Converter />
+                </Col>
+            </Row>
         </div>
     );
 }
